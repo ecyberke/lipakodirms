2271bbab29ea2fba69e2942d610bc45d371d08e7
Eric Ribia <eric.ndungu16@gmail.com>
2026-04-03 12:17:52 -0400

    Fix: CSRF re-enabled, database sessions, login redirect, M-Pesa routes excluded from CSRF

---
+++ storage/framework/views/5ed71f9a50b15a5570a4ba2070652deb.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<!-- Meta data -->
+		<meta charset="UTF-8">
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+		<meta content="Lipakodi Property Management System - Property Management System" name="description">
+		<meta content="author" name="Bedah">
+		<meta name="keywords" content="The best rent management system that enables one to have access to all payments, real-time monitoring of units and properties and records of all tenants who get alerts via sms when their rents are due and overdue."/>
+		<?php echo $__env->make('layouts.custom-head', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+	</head>
+		
+	<body class="h-100vh page-style1 light-mode default-sidebar">	    
+		<?php echo $__env->yieldContent('content'); ?>		
+		<?php echo $__env->make('layouts.custom-footer-scripts', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>	
+	</body>
+</html><?php /**PATH /home/ecyberco/domains/lipakodi.ecyber.co.ke/public_html/rms/resources/views/layouts/master2.blade.php ENDPATH**/ ?>
